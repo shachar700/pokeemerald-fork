8024c56212e1abbb19328e029b31fea303df97ed
RubyRaven6 <swampertspawner@gmail.com>
2025-01-10 15:02:02 +0800

    Refined Chii Town NPCs; made schedules for school.

--- include/constants/rtc.h
+++ include/constants/rtc.h
@@ -1,5 +1,3 @@
-#include "vars.h"
-
 #define TIME_DEAD_NIGHT         0
 #define TIME_EARLY_MORNING      1
 #define TIME_MORNING            2
@@ -7,8 +5,3 @@
 #define TIME_NOONTIME           4
 #define TIME_EVENING            5
 #define TIME_NIGHT              6
-
-#define VAR_TIME_OF_DAY         VAR_0x8000
-
-#define VAR_TIME_HOUR           VAR_0x8000
-#define VAR_TIME_MINUTE         VAR_0x8001
