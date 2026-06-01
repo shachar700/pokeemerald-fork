55fbb31adf0685c5f5a2b85a2f71eb303bfc702e
shachar700 <shachar700@gmail.com>
2026-06-01 19:01:59 +0300

    v1.0.9-wip: trainer hill translations

--- src/trainer_hill.c
+++ src/trainer_hill.c
@@ -378,16 +378,16 @@ void CopyTrainerHillTrainerText(u8 which, u16 localId)
     switch (which)
     {
     case TRAINER_HILL_TEXT_INTRO:
-        FrontierSpeechToString(sHillData->floors[floorId].trainers[id].speechBefore);
+        FrontierSpeechToString((const u16 *)sHillData->floors[floorId].trainers[id].speechBefore);
         break;
     case TRAINER_HILL_TEXT_PLAYER_LOST:
-        FrontierSpeechToString(sHillData->floors[floorId].trainers[id].speechWin);
+        FrontierSpeechToString((const u16 *)sHillData->floors[floorId].trainers[id].speechWin);
         break;
     case TRAINER_HILL_TEXT_PLAYER_WON:
-        FrontierSpeechToString(sHillData->floors[floorId].trainers[id].speechLose);
+        FrontierSpeechToString((const u16 *)sHillData->floors[floorId].trainers[id].speechLose);
         break;
     case TRAINER_HILL_TEXT_AFTER:
-        FrontierSpeechToString(sHillData->floors[floorId].trainers[id].speechAfter);
+        FrontierSpeechToString((const u16 *)sHillData->floors[floorId].trainers[id].speechAfter);
         break;
     }
 
